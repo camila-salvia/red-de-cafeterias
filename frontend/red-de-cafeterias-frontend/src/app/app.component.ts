55353aae98702b636d7edb38c057bd9ab84d2670
Camila Salvia <camilasalvia2@gmail.com>
2025-08-20 15:53:52 -0300

    se crean los componentes iniciales

--- frontend/red-de-cafeterias-frontend/src/app/app.component.ts
+++ frontend/red-de-cafeterias-frontend/src/app/app.component.ts
@@ -3,10 +3,12 @@
 
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./features/home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, 
+    HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ frontend/red-de-cafeterias-frontend/src/app/shared/botones/botones.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-botones',
+  imports: [],
+  templateUrl: './botones.component.html',
+  styleUrl: './botones.component.css'
+})
+export class BotonesComponent {
+
+}
