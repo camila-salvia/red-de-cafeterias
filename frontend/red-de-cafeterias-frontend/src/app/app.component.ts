553c355f8253a93ce3cddc3b46a94f0c1db8ce07
Camila Salvia <camilasalvia2@gmail.com>
2025-08-20 19:25:43 -0300

    se creó la barra de navegación

--- frontend/red-de-cafeterias-frontend/src/app/app.component.ts
+++ frontend/red-de-cafeterias-frontend/src/app/app.component.ts
@@ -4,11 +4,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./features/login/login.component";
+import { HomeComponent } from "./features/home/home.component";
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,
-    LoginComponent],
+    LoginComponent, 
+    HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
