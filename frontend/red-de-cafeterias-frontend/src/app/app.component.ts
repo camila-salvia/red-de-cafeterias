1465ce30cd9a4415abe0c995fe7326ac334a9fb0
Camila Salvia <camilasalvia2@gmail.com>
2025-08-20 15:03:52 -0300

    se agregan comentarios para entender el funcionamiento inicial

--- frontend/red-de-cafeterias-frontend/src/app/app.component.ts
+++ frontend/red-de-cafeterias-frontend/src/app/app.component.ts
@@ -1,3 +1,6 @@
+// Este es el componente angular de nivel superior de la aplicación
+// En el .ts se escribe el código fuente que describe el componente
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
--- frontend/red-de-cafeterias-frontend/src/main.ts
+++ frontend/red-de-cafeterias-frontend/src/main.ts
@@ -1,3 +1,5 @@
+// Es donde la aplicación comienza a ejecutarse
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
