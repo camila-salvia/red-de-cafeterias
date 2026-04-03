8cde1d87e6cca2f33f46cdd3c5217b6b33d2a672
Martinvivas <129123814+Martinvivas@users.noreply.github.com>
2026-04-03 16:36:26 -0300

    ?

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import categoriaRoutes from './categoria/categoria.routes.js';
+
 const app = express();
 
 // Middleware para JSON
 app.use(express.json());
 
-// Rutas
+// Rutas 
 app.use('/api/categoria', categoriaRoutes);
 
 // Health / root
