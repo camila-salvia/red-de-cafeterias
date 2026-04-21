928e32a63ec953755ec36081b65b3f48232902c9
marileonardelli <marileonardelli@hotmail.com>
2026-04-21 16:02:49 -0300

    usuario.entity

---
+++ backend/src/usuario/usuario.entity.ts
@@ -0,0 +1,39 @@
+import {
+    Entity,
+    Property,
+    OneToMany,
+    Collection,
+} from '@mikro-orm/core'
+import { BaseEntity } from '../shared/database/baseEntity.entity.js'
+// import { Comentario } from '../comentario/comentario.entity.js'
+// import { Pedido } from '../pedido/pedido.entity.js'
+
+@Entity()
+export class Usuario extends BaseEntity {
+    @Property({nullable: false})
+    nombre!: string
+
+    @Property({ nullable: false })
+    apellido!: string
+
+    @Property({ nullable: false })
+    telefono!: number
+
+    @Property({ nullable: false})
+    direccion!: string
+
+    @Property({ nullable: false, unique: true })
+    email!: string
+
+    @Property({ nullable: false })
+    password!: string
+
+    @Property({ nullable: false })
+    esAdmin: boolean = false
+
+    // @OneToMany(() => Comentario, comentario => comentario.usuario)
+    // comentarios = new Collection<Comentario>(this)
+
+    // @OneToMany(() => Pedido, pedido => pedido.usuario)
+    // pedidos = new Collection<Pedido>(this)
+}
