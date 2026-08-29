53a6bab2acdc417769cb9ca1ad42eab6ced36180
marileonardelli <marileonardelli@hotmail.com>
2026-08-29 17:58:29 -0300

    inicio sesion front

--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-import { FormularioLoginComponent } from '../../components/formulario-login/formulario-login';
+import { Component, signal } from '@angular/core';
+import {CommonModule} from '@angular/common';
+//import { FormularioLoginComponent } from '../../components/formulario-login/formulario-login';
 
 @Component({
   selector: 'app-login',
-  imports: [FormularioLoginComponent],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  vistaLogin = signal<boolean>(true);
+
+  toggleVista() {
+    this.vistaLogin.set(!this.vistaLogin());
+  }
+}
