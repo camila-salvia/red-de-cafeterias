d5f341a2f49d291b65d7553148bad3c6ed79d8b8
Camila Salvia <camilasalvia2@gmail.com>
2026-09-04 14:45:11 -0300

    estilos

--- frontend/src/app/components/boton/boton.ts
+++ frontend/src/app/components/boton/boton.ts
@@ -1,11 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-boton',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './boton.html',
   styleUrl: './boton.scss',
 })
 export class BotonComponent {
   @Input() texto: string = '';
+  @Input() variante: 'primario' | 'logout' = 'primario';
+  @Output() onClick = new EventEmitter<void>();
 }
