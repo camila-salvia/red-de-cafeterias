eab87636f6caf44a9d24a3ed1f6c0c1bc0f21e30
Camila Salvia <camilasalvia2@gmail.com>
2026-09-04 14:15:25 -0300

    ver solo pedido confirmado y listado de pedidos historico

--- frontend/src/app/pages/ver-pedido/ver-pedido.ts
+++ frontend/src/app/pages/ver-pedido/ver-pedido.ts
@@ -1,5 +1,5 @@
-import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { CommonModule, DatePipe } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
 import { CartService } from '../../services/cart.js';
 import { AuthService } from '../../services/auth';
@@ -8,7 +8,7 @@ import { ApiService } from '../../services/api';
 @Component({
   selector: 'app-ver-pedido',
   standalone: true,
-  imports: [ CommonModule, RouterLink],
+  imports: [ CommonModule, RouterLink, DatePipe],
   templateUrl: './ver-pedido.html',
   styleUrl: './ver-pedido.scss',
 })
@@ -16,6 +16,9 @@ export class VerPedidoComponent {
   cartService = inject(CartService);
   private apiService = inject(ApiService);
   private router = inject(Router);
+
+  // pedido recien confirmado
+pedidoReciente = signal<any | null>(null);
 
   incrementar(id?: number | string) {
   if (id !== undefined) {
@@ -37,34 +40,47 @@ export class VerPedidoComponent {
 
   confirmarPedido() {
     if (this.cartService.items().length === 0) return;
-    const usuarioId = localStorage.getItem('usuario_id') || '1'; // Ajusta según cómo guardes el ID en login
+    const usuarioId = localStorage.getItem('usuario_id');
+    if (!usuarioId) {
+      alert('Debes iniciar sesión.');
+      this.router.navigate(['/login']);
+      return;
+    }
+
+    // para el comprobante
+  const itemsComprados = [...this.cartService.items()];
 
   const pedidoPayload = {
     usuario: usuarioId,
-    metodo_pago: "6748ac98-6103-4965-a17d-6b3fff777eea", // ID del método de pago por defecto - tarjeta
-    direccion_envio: 'Retiro en sucursal',
+    metodo_pago: "6748ac98-6103-4965-a17d-6b3fff777eea", // por defecto - tarjeta
+    direccion_envio: 'Retiro en sucursal', // por defecto
     fecha_pedido: new Date(),
     fecha_pago: new Date(),
-    estado_pago: 'Aprobado',
+    estado_pago: 'Aprobado', // checkear
     costo_total: this.cartService.precioTotal(),
-    items: this.cartService.items().map(item => ({
-      productoId: item.producto.id,
-      cantidad: item.cantidad
-    }))
+    items: itemsComprados.map(item => ({
+        productoId: item.producto.id,
+        cantidad: item.cantidad
+      }))
   };
 
   this.apiService.crearPedido(pedidoPayload).subscribe({
     next: (res: any) => {
-      alert('¡Pedido confirmado con éxito!');
+      const nuevoPedido = res.data ? res.data : res;
+      this.pedidoReciente.set({
+        ...nuevoPedido,
+        itemsVisuales: itemsComprados
+      });
       this.cartService.limpiarCarrito();
-      this.router.navigate(['/mis-pedidos']);
     },
     error: (err) => {
-      console.error('Error completo:', err);
-      console.error('Status HTTP:', err.status);
-      console.error('Mensaje del Backend:', err.error);
-      alert('Ocurrió un error al procesar tu pedido. Revisa la consola.');
-}
+      console.error('Error al crear pedido', err);
+      alert('Ocurrió un error al procesar tu pedido.');
+    }
   });
+}
+volverAlMenu() {
+  this.pedidoReciente.set(null);
+  this.router.navigate(['/']);
 }
 }
